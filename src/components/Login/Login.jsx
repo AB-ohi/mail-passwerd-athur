c1fb8bf94e9e704847f9b35a3326ddbc84a46d8a
AB-ohi <askohi63907@gmail.com>
2023-04-17 03:01:11 +0600

    error i can not solved

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 
 const Login = () => {
+    const [error,setError] = ('')
+    const [success, setSuccess] = ('');
+
+    const handelLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+        console.log(email,password)
+
+        setError('');
+        setSuccess('');
+
+
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('Please add at least two uppercase.');
+            return;
+        }
+        else if (!/(?=.*[!@#$&*])/.test(password)) {
+            setError('Please add a special character.');
+            return;
+        }
+        else if (password.length < 6) {
+            setError('Password must be 6 characters long');
+            return;
+        }
+    }
     return (
         <div>
-            
+            <h2>Please login</h2>
+            <form onSubmit={handelLogin}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input type="text"  name='email' className="form-control" id="username" placeholder="Enter your username" required />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control" id="password" placeholder="Enter your password" required/>
+                </div>
+                <p className='text-danger'>{error}</p>
+                <button type="submit" className="btn btn-primary">Log In</button>
+            </form>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,48 +1,100 @@
 import React, { useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from 'firebase/auth'
+// import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth;
 
-
-
 const Register = () => {
-    const [email, setEmail] = useState();
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-     const handelSubmit = (event) =>{
+    const handleSubmit = (event) => {
+        // 1. prevent page refresh
         event.preventDefault();
-        const email = event.target.email.value
-        const password = event.target.password.value
-        console.log(email, password)
+        setSuccess('');
+        setError('');
+        // 2. collect form data
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const name = event.target.name.value;
+        console.log(name, email, password)
 
+        // validate
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Please add at least one uppercase');
+            return;
+        }
+        else if (!/(?=.*[0-9].*[0-9])/.test(password)) {
+            setError('Please add at least two numbers');
+            return
+        }
+        else if (password.length < 6) {
+            setError('Please add at least 6 characters in your password')
+            return;
+        }
+
+        // 3. create user in fb
         createUserWithEmailAndPassword(auth, email, password)
-        .then()
-        .catch(error => {
-            console.error(error)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                setError('');
+                event.target.reset();
+                setSuccess('User has been created successfully');
+                sendVerificationEmail(result.user);
+                updateUserData(result.user, name);
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
+    }
+
+    const sendVerificationEmail = (user) => {
+        sendEmailVerification(user)
+            .then(result => {
+                console.log(result);
+                alert('Please verify your email address')
+            })
+    }
+
+    const updateUserData = (user, name) => {
+        updateProfile(user, {
+            displayName: name
         })
-     }
-
-    const handelEmailChange = (event) => {
-        // console.log(event.target.value)
-        setEmail (event.target.value)
-    }
-    const handelPasswordBlur = (event) => {
-        // console.log(event.target.value)
-        
+            .then(() => {
+                console.log('user name updated')
+            })
+            .catch(error => {
+                setError(error.message);
+            })
     }
 
+    const handleEmailChange = (event) => {
+        // console.log(event.target.value);
+        // setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = (event) => {
+        // console.log(event.target.value);
+    }
 
     return (
-        <div>
-            <h2>
-                Register-this-page
-            </h2>
-            <form onSubmit={handelSubmit}>
-                <input className='w-50 mb-4 rounded ps-2 bs-border-color-rgb ' onChange={handelEmailChange} type="email" name="email" id="email" placeholder='inter your mail' />
+        <div className='w-50 mx-auto'>
+            <h4>Please Register</h4>
+            <form onSubmit={handleSubmit}>
+                <input className='w-50 mb-4 rounded ps-2' type="text" name="name" id="name" placeholder='Your Name' required />
                 <br />
-                <input className='w-50 mb-4 rounded ps-2 bs-border-color-rgb ' onBlur={handelPasswordBlur} type="password" name="password" id="password" placeholder='your password' />
+                <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' required />
                 <br />
-                <input className='' type="submit" value="register" />
+                <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
+                <br />
+                <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+            <p><small>Already have an account? Please <Link to="/login">Login</Link> </small></p>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
